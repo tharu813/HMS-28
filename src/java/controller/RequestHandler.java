36953bab2e593fbdb71690f9ccbed3f5ff2a83ce
tharu813 <kav.ds8@gmail.com>
2021-05-01 17:39:00 +0530

    Adding sompleted request handler

--- src/java/controller/RequestHandler.java
+++ src/java/controller/RequestHandler.java
@@ -6,6 +6,7 @@
 package controller;
 
 import javax.servlet.http.HttpServletRequest;
+import model.User;
 
 /**
  *
@@ -13,14 +14,35 @@ import javax.servlet.http.HttpServletRequest;
  */
 public class RequestHandler {
     
+    // Adds an attribute to the current request
     public static void addAttribute(HttpServletRequest req, String key, Object val){
         req.getSession().setAttribute(key, val);
     }
     
+    /* Returns the required attribute and removes it from the current session.
+    Use this when processing the required object gets completed after this request. 
+    */
     public static Object retrieveAttribute(HttpServletRequest req, String key) {
         Object val = req.getSession().getAttribute(key);
         req.getSession().removeAttribute(key);
         return val;
+    }
+    
+    /* Returns the required attribute without removing it from the current session.
+    Use this when the required attribute needs to be forwarded in following requests. 
+    */
+    public static Object fetchAttribute(HttpServletRequest req, String key) {
+        Object val = req.getSession().getAttribute(key);
+        return val;
+    }
+    
+    /*
+    Return the currently logged in user. 
+    Need to cast if child class capabilities are required.
+    */
+    public static User getCurrentUser(HttpServletRequest req) {
+        User user = (User) req.getSession().getAttribute("user");
+        return user;
     }
     
 }
