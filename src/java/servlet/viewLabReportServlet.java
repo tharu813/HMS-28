4d50e9e5126d308183e70f2405530782527a82ee
tharu813 <kav.ds8@gmail.com>
2021-05-12 19:21:42 +0530

    Improved and fixed View Lab Report

--- src/java/servlet/viewLabReportServlet.java
+++ src/java/servlet/viewLabReportServlet.java
@@ -1,19 +1,28 @@
 package servlet;
 
+import model.MedicalRecord;
+import service.PatientService;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "viewLabReportServlet")
 public class viewLabReportServlet extends HttpServlet {
+    PatientService ps= new PatientService();
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        HttpSession session = request.getSession(false);
+        String user_id = (String)session.getAttribute("user_id");
+        System.out.println("servlet" +user_id);
+        MedicalRecord m = new MedicalRecord();
+        m = ps.viewMedicalReport(user_id);
     }
 }
