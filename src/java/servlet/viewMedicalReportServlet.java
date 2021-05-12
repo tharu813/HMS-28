fafc94b1dec589854938b93656d4e513e9f4881a
tharu813 <kav.ds8@gmail.com>
2021-05-12 19:21:59 +0530

    Improved and fixed View Medical Report

--- src/java/servlet/viewMedicalReportServlet.java
+++ src/java/servlet/viewMedicalReportServlet.java
@@ -1,19 +1,35 @@
 package servlet;
 
+import model.MedicalRecord;
+import model.Patient;
+import service.PatientService;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "viewMedicalReportServlet")
 public class viewMedicalReportServlet extends HttpServlet {
+    PatientService ps = new PatientService();
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String patientId=request.getParameter("patientId");
+        HttpSession session = request.getSession(false);
+        String user_id = (String)session.getAttribute("user_id");
+        System.out.println("servlet" +user_id);
+        MedicalRecord m = new MedicalRecord();
+        m = ps.viewMedicalReport(user_id);
+
+
+
+        RequestDispatcher ds =request.getRequestDispatcher("viewPatientProfile.jsp");
+        ds.forward(request,response);
     }
 }
