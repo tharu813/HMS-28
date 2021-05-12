3b7f2a2277e8805bd041e036ec2de55eb7956c62
tharu813 <kav.ds8@gmail.com>
2021-05-12 19:22:15 +0530

    Improved and fixed View Prescription

--- src/java/servlet/viewPrescriptionServlet.java
+++ src/java/servlet/viewPrescriptionServlet.java
@@ -1,19 +1,34 @@
 package servlet;
 
+import model.Patient;
+import model.Prescription;
+import service.PatientService;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet(name = "viewPrescriptionServlet")
+@WebServlet("/viewPrescriptionServlet")
 public class viewPrescriptionServlet extends HttpServlet {
+    PatientService ps = new PatientService();
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
+        String user_id = (String)session.getAttribute("user_id");
+        System.out.println("servlet" +user_id);
+        Prescription p = new Prescription();
+        p = ps.viewPrescription(user_id);
 
+
+        RequestDispatcher ds =request.getRequestDispatcher("viewPatientProfile.jsp");
+        ds.forward(request,response);
     }
 }
