094cc63b1e424c8f9bae6e0c1f3554e105e56450
tharu813 <kav.ds8@gmail.com>
2021-05-12 19:24:06 +0530

    Improved and fixed methods in - Patient Service

--- src/java/service/PatientService.java
+++ src/java/service/PatientService.java
@@ -22,26 +22,26 @@ public class PatientService  implements  IPatient{
         try {
 
             con = DBConnectionUtil.getConnection();
-            String sql = "SELECT * from user u,patient p,user_contact uc where u.user_id=p.patient_id and uc.user_id=u.user_id";
-
+            String sql = "SELECT * from user u,patient p,user_contact uc where u.user_id=p.patient_id and uc.user_id=u.user_id and u.user_id = ?;";
             preparedStatement = con.prepareStatement(sql);
-            System.out.println(preparedStatement);
+            preparedStatement.setString(1, user_id);
 
             ResultSet resultSet = preparedStatement.executeQuery();
            while(resultSet.next()) {
-               p.setUserId(resultSet.getString(1));
-               p.setUsername(resultSet.getString(2));
-               p.setNic(resultSet.getString(3));
-               p.setFirstName(resultSet.getString(4));
-               p.setLastName(resultSet.getString(5));
-               p.setEmail(resultSet.getString(6));
-               p.setUserId(resultSet.getString(7));
-               p.setAge(Integer.parseInt(resultSet.getString(8)));
-               p.setDob(Date.valueOf(resultSet.getString(9)));
-               p.setHouse_no(Integer.parseInt(resultSet.getString(10)));
-               p.setStreet(resultSet.getString(11));
-               p.setCity(resultSet.getString(12));
-               p.setRegistered_by(resultSet.getString(13));
+               p.setUserId(resultSet.getString("user_id"));
+               p.setUsername(resultSet.getString("username"));
+               p.setNic(resultSet.getString("nic"));
+               p.setFirstName(resultSet.getString("first_name"));
+               p.setLastName(resultSet.getString("last_name"));
+               p.setEmail(resultSet.getString("email"));
+               //p.setUserId(resultSet.getString(7));
+               p.setAge(Integer.parseInt(resultSet.getString("age")));
+               p.setDob(resultSet.getDate("dob"));
+               p.setHouse_no(Integer.parseInt(resultSet.getString("house_no")));
+               p.setStreet(resultSet.getString("street"));
+               p.setCity(resultSet.getString("city"));
+               p.setRegistered_by(resultSet.getString("registered_by"));
+               p.setContactNo(resultSet.getString("contact_no"));
 
            }
             System.out.println("service"+user_id);
@@ -67,10 +67,10 @@ public class PatientService  implements  IPatient{
         try {
 
             con = DBConnectionUtil.getConnection();
-            String sql = "SELECT * from medical_record m,patient p,u user where u.user_id=p.patient_id and p.patient_id=m.patient_id";
+            String sql = "SELECT * from medical_record m,patient p,user u where u.user_id=p.patient_id and p.patient_id=m.patient_id and u.user_id = ?;";
 
             preparedStatement = con.prepareStatement(sql);
-            System.out.println(preparedStatement);
+            preparedStatement.setString(1, user_id);
 
             ResultSet resultSet = preparedStatement.executeQuery();
             while(resultSet.next()) {
@@ -80,8 +80,8 @@ public class PatientService  implements  IPatient{
                 m.setHeight(resultSet.getInt(4));
                 m.setAllergies(resultSet.getString(5));
                 m.setDiagnoses(resultSet.getString(6));
-                m.setMedication(resultSet.getString(8));
-                m.setPatient_id(resultSet.getString(9));
+                m.setMedication(resultSet.getString(7));
+                m.setPatient_id(resultSet.getString(8));
 
             }
             System.out.println("service"+user_id);
@@ -102,7 +102,7 @@ public class PatientService  implements  IPatient{
         try {
 
             con = DBConnectionUtil.getConnection();
-            String sql = "SELECT * from lab_report_details l,patient p,u user where u.user_id=p.patient_id and p.patient_id=l.patient_id";
+            String sql = "SELECT * from lab_report_details l,patient p,user u where u.user_id=p.patient_id and p.patient_id=l.patient_id";
 
             preparedStatement = con.prepareStatement(sql);
             System.out.println(preparedStatement);
@@ -132,7 +132,32 @@ public class PatientService  implements  IPatient{
     }
 
     @Override
-    public Prescription viewPrescription() {
-        return null;
+    public Prescription viewPrescription(String user_id) {
+        Prescription p = new Prescription();
+        try {
+            String sql = "SELECT * from prescription m,patient p,user u where u.user_id=p.patient_id and p.patient_id=m.prescribed_for and u.user_id = ?;";
+
+            preparedStatement = con.prepareStatement(sql);
+            preparedStatement.setString(1, user_id);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while(resultSet.next()) {
+                p.setPrescription_id(resultSet.getInt(1));
+                p.setDiagnosis(resultSet.getString(2));
+                p.setReason(resultSet.getString(3));
+                p.setFurther_treatment(resultSet.getString(4));
+                p.setPrescribed_for(resultSet.getString(5));
+                p.setPrescribed_by(resultSet.getString(6));
+
+
+
+            }
+
+
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+        return p;
     }
+
 }
